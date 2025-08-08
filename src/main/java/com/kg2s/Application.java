8263b0b7d9784939a4eab13f890c0d83e51f3adf
zyfAnd <zhangyanfu66@gmail.com>
2025-08-08 16:01:49 +0800

    Updated desc

--- src/main/java/com/kg2s/Application.java
+++ src/main/java/com/kg2s/Application.java
@@ -1,17 +1,18 @@
 package com.kg2s;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author Yanfu Zhang
  * @date 2025-08-08
- * @description
+ * @description Weather RESTful APIs Application
  */
 @SpringBootApplication
+@EnableFeignClients
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-}
+} 
