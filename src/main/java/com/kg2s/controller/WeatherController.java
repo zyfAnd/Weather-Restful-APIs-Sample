48c354ea808d990e04bbe568f4638ae13bdfe99a
zyfAnd <zhangyanfu66@gmail.com>
2025-08-08 15:33:51 +0800

    Updated

--- src/main/java/com/kg2s/controller/WeatherController.java
+++ src/main/java/com/kg2s/controller/WeatherController.java
@@ -1,6 +1,7 @@
 package com.kg2s.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.kg2s.domain.WeatherInfoResp;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Yanfu Zhang
@@ -8,5 +9,37 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  */
 @RestController
+@RequestMapping("/weather")
 public class WeatherController {
+
+
+    /**
+     *
+     * @param city
+     * @param country
+     * @return
+     *
+     * desc that is for standard Restful APIs - path vars
+     */
+    @GetMapping("/query/{city}/{country}")
+    @ResponseBody
+    public WeatherInfoResp queryWeather(@PathVariable("city") String city, @PathVariable("country") String country){
+        System.out.println("WeatherInfo..");
+        return new WeatherInfoResp();
+    }
+    /**
+     *
+     * @param city
+     * @param country
+     * @return
+     *
+     * desc that is for request params
+     */
+    @GetMapping("/query")
+    @ResponseBody
+    public WeatherInfoResp selectWeather(@RequestParam("city") String city, @RequestParam("country") String country){
+        System.out.println("selectWeather..");
+        return new WeatherInfoResp();
+    }
+
 }
---
+++ src/main/java/com/kg2s/domain/WeatherInfoResp.java
@@ -0,0 +1,29 @@
+package com.kg2s.domain;
+
+/**
+ * @author Yanfu Zhang
+ * @date 2025-08-08
+ * @description
+ */
+public class WeatherInfoResp {
+    String id ;
+    String main;
+    String description;
+    String icon;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+}
